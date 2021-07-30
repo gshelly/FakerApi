abd09b4cff6dda890ff6f3f260ea02beb6c3f306
gshelly <gshelly1402@gmail.com>
2021-07-29 18:43:05 -0700

    created Faker Api and run using nodemon

---
+++ FakerApi.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const faker = require('faker')
+const app = express()
+const port = 8000;
+
+app.listen(port, () => {
+  console.log("Listening to Port", port);
+})
+
+const createUser = () => {
+  const newUser = {
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    phoneNumber: faker.phone.phoneNumber(),
+    email: faker.internet.email(),
+    password: faker.internet.password()
+  }
+  return newUser;
+}
+
+const company = () => {
+  const newCompany = {
+    name: faker.company.companyName(),
+    address: getAddress()
+  }
+  return newCompany;
+}
+
+const getAddress = () => {
+  const address = {
+    street: faker.address.streetName(),
+    city: faker.address.city(),
+    state: faker.address.state(),
+    zipCode: faker.address.zipCode(),
+    country: faker.address.country()
+  }
+  return address;
+}
+
+app.get("/api/user", (req, res) => {
+  res.json(createUser())
+})
+
+app.get("/api/company", (req, res) => {
+  res.json(company())
+})
